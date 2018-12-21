eba5985053d7e26dfacfbbd97ebcb3f1968952e4
Patrick <pjeschba@umich.edu>
2018-12-20 22:45:17 -0500

    First commit

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_flatpages import FlatPages
+
+FLATPAGES_EXTENSION = '.md'
+
+app = Flask(__name__)
+Bootstrap(app)
+app.config.from_pyfile('settings.py')
+pages = FlatPages(app)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', pages=pages)
+
+
+@app.route('/contact/')
+def contact():
+    return render_template('contact.html')
+
+
+@app.route('/resume/')
+def resume():
+    return render_template('resume.html')
+
+
+@app.route('/<path:path>/')
+def page(path):
+    page = pages.get_or_404(path)
+    return render_template('page.html', page=page)
