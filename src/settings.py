5329c0857a9844768713856549978fc5ebc9f6bf
Patrick <pjeschba@umich.edu>
2018-12-23 18:28:50 -0500

    Added favicon

--- src/settings.py
+++ src/settings.py
@@ -1,28 +1,22 @@
 import os
 
-# REPO_NAME = "pjeschba.github.io"  # Used for FREEZER_BASE_URL
-DEBUG = True
 
-# Assumes the app is located in the same directory
-# where this file resides
-APP_DIR = os.path.dirname(os.path.abspath(__file__))
-
-
+# General
 def parent_dir(path):
     '''Return the parent of a directory.'''
     return os.path.abspath(os.path.join(path, os.pardir))
 
 
+DEBUG = True
+APP_DIR = os.path.dirname(os.path.abspath(__file__))
 PROJECT_ROOT = parent_dir(APP_DIR)
-# In order to deploy to Github pages, you must build the static files
-# to
-# the project root
+
+
+# Flask freeze config
 FREEZER_DESTINATION = PROJECT_ROOT
-# Since this is a repo page (not a Github user page),
-# we need to set the BASE_URL to the correct url as per GH Pages'
-# standards
-# FREEZER_BASE_URL = "https://github.com/pjeschba/{0}".format(REPO_NAME)
 FREEZER_BASE_URL = "https://patrickeschbach.com"
-FREEZER_REMOVE_EXTRA_FILES = False  # IMPORTANT: If this is True, all app files will be deleted when you run the freezer
+FREEZER_REMOVE_EXTRA_FILES = False  # Needed since we build in proj_root
+
+# Flatpages config
 FLATPAGES_ROOT = os.path.join(APP_DIR, 'pages')
 FLATPAGES_EXTENSION = '.md'
